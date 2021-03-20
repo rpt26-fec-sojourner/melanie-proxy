2a0753b9df1d473ba2329a2ee8ea68a023233b8b
Melanie DeWitt <melaniebd1986@gmail.com>
2021-03-20 14:52:23 -0600

    proxy rendering my service (need HTML dist files for others), can switch between properties

---
+++ client/src/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import $ from 'jquery';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 9001;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('client/src'));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.listen(port, () => {
+  console.log(`listening at port ${port}`);
+});
+
+app.get('/:id', (req, res) => {
+  console.log('this is being called')
+  res.header('Access-Control-Allow-Origin', '*');
+
+  res.sendFile(__dirname + '/client/src/index.html')
+});
